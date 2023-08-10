d35a9d94dba25ae9c73eb799b8ae3b2f9ca1f940
Jacob Sikorski <jacob.sikorski@gmail.com>
2023-08-09 18:51:54 -0600

    Add query param stripping iOS APIs

---
+++ ios/browser/api/query_filter/query_filter_service.h
@@ -0,0 +1,36 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_IOS_BROWSER_API_QUERY_FILTER_QUERY_FILTER_SERVICE_H_
+#define BRAVE_IOS_BROWSER_API_QUERY_FILTER_QUERY_FILTER_SERVICE_H_
+
+#import <Foundation/Foundation.h>
+
+OBJC_EXPORT
+@interface QueryFilterService : NSObject
+- (instancetype)init NS_UNAVAILABLE;
+
+/**
+ This function will return a new url stripping any tracking query params.
+ If nothing is to be stripped, a null value is returned.
+
+ @param requestURL Indicates where we are navigating to.
+ @param initiatorURL Indicates the origin initiating the resource request. It's
+ the first url in a redirect chain.
+ @param redirectSourceURL Indicates the we are currently navigating from. It's
+ the last url in a redirect chain
+ @param method The HTTP method of the request.
+ @param isInternalRedirect Indicating if this is an internal redirect or not.
+ @return The url we should redirect to.
+ */
++ (NSURL*)stripQueryParamsFromRequestURL:(NSURL*)requestURL
+                            initiatorURL:(NSURL*)initiatorURL
+                       redirectSourceURL:(NSURL*)redirectSourceURL
+                           requestMethod:(NSString*)requestMethod
+                      isInternalRedirect:(BOOL)isInternalRedirect;
+
+@end
+
+#endif  // BRAVE_IOS_BROWSER_API_QUERY_FILTER_QUERY_FILTER_SERVICE_H_
---
+++ ios/browser/api/query_filter/query_filter_service.mm
@@ -0,0 +1,37 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/ios/browser/api/query_filter/query_filter_service.h"
+
+#include <string>
+#include "base/strings/sys_string_conversions.h"
+#include "brave/components/query_filter/utils.h"
+#import "net/base/mac/url_conversions.h"
+#include "url/gurl.h"
+
+@implementation QueryFilterService
+
++ (NSURL*)stripQueryParamsFromRequestURL:(NSURL*)requestURL
+                            initiatorURL:(NSURL*)initiatorURL
+                       redirectSourceURL:(NSURL*)redirectSourceURL
+                           requestMethod:(NSString*)requestMethod
+                      isInternalRedirect:(BOOL)isInternalRedirect {
+  GURL initiator_gurl = net::GURLWithNSURL(initiatorURL);
+  GURL redirect_source_gurl = net::GURLWithNSURL(redirectSourceURL);
+  GURL request_gurl = net::GURLWithNSURL(requestURL);
+  const auto request_method = base::SysNSStringToUTF8(requestMethod);
+
+  auto filtered_url = query_filter::MaybeApplyQueryStringFilter(
+      initiator_gurl, redirect_source_gurl, request_gurl, request_method,
+      isInternalRedirect);
+
+  if (filtered_url.has_value()) {
+    return net::NSURLWithGURL(filtered_url.value());
+  } else {
+    return requestURL;
+  }
+}
+
+@end
